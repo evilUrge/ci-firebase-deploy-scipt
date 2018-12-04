f02a6185fe7c70a553fb09b8826b6c0904f3f348
Gilad Maoz <gilad.maoz@gmail.com>
2018-12-04 13:32:44 +0100

    Update fb-tools to the latest Added autodetect for project name and type (Inc 'only' flag with the new format)

--- deploy.js
+++ deploy.js
@@ -1,23 +1,39 @@
-let fs = require('fs');
-let token = fs.readFileSync(`${__dirname}/.firebase-token`);
-let firebaseSiteConf = Object.keys(require('./firebase.json'));
-let projectName = (JSON.parse(fs.readFileSync(`${__dirname}/.firebaserc`, 'utf8'))).projects[process.env.NODE_ENV];
+const fs = require('fs');
+const token = fs.readFileSync(`${__dirname}/.firebase-token`);
+const firebaseSiteConf = Object.keys(require('./firebase.json'));
+const functionName = require('./package').name;
+const projectName = (JSON.parse(fs.readFileSync(`${__dirname}/.firebaserc`, 'utf8'))).projects[process.env.NODE_ENV];
 /**
-* FireBase Deployment script
+ * FireBase Deployment script
  * FOR CI USE ONLY!
  */
 
 console.log('Deploying to Firebase... please wait.');
-require('firebase-tools').deploy({
-    only_functions: firebaseSiteConf.length === 1 && firebaseSiteConf[0] === 'functions',
-    only_hosting: firebaseSiteConf.length === 1 && firebaseSiteConf[0] === 'hosting',
-    only_database: firebaseSiteConf.length === 1 && firebaseSiteConf[0] === 'database',
-    project: projectName,
-    token: token,
-    cwd: __dirname,
-}).then(() => {
+
+require('firebase-tools').deploy((() => {
+    let fbServices = ['functions', 'hosting', 'database'];
+
+    let baseConf = {
+        project: projectName,
+        token: token,
+        cwd: __dirname,
+    };
+
+    for (let serviceIndex in fbServices) {
+        let currentService = fbServices[serviceIndex];
+        if (firebaseSiteConf.includes(currentService)) {
+            if (Object.keys(baseConf).includes('only')) {
+                delete baseConf['only'];
+                break;
+            } else {
+                baseConf['only'] = `${currentService}:${functionName}`;
+            }
+        }
+    }
+    return baseConf;
+})()).then(() => {
     console.log(`Your project ${projectName} has been deployed`);
-    return true
+    return true;
 }).catch((err) => {
     console.warn(`error has occurred! - ${err}`);
 });
