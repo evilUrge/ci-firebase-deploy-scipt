6d83c66d88d408432db8bdfc03b4d057a8958ea9
Gilad Maoz <gilad.maoz@gmail.com>
2019-01-25 20:26:28 +0100

    Simplified deploy.js

--- deploy.js
+++ deploy.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const token = fs.readFileSync(`${__dirname}/.firebase-token`);
-const firebaseSiteConf = Object.keys(require('./firebase.json'));
-const functionName = require('./package').name;
-const projectName = (JSON.parse(fs.readFileSync(`${__dirname}/.firebaserc`, 'utf8'))).projects[process.env.NODE_ENV];
+const projectName = (JSON.parse(fs.readFileSync(`${__dirname}/.firebaserc`, 'utf8'))).projects[process.env.NODE_ENV ? process.env.NODE_ENV : 'default'];
 /**
  * FireBase Deployment script
  * FOR CI USE ONLY!
@@ -10,30 +8,7 @@ const projectName = (JSON.parse(fs.readFileSync(`${__dirname}/.firebaserc`, 'utf
 
 console.log('Deploying to Firebase... please wait.');
 
-require('firebase-tools').deploy((() => {
-    let fbServices = ['functions', 'hosting', 'database'];
-
-    let baseConf = {
-        project: projectName,
-        token: token,
-        cwd: __dirname,
-    };
-
-    for (let serviceIndex in fbServices) {
-        let currentService = fbServices[serviceIndex];
-        if (firebaseSiteConf.includes(currentService)) {
-            if (Object.keys(baseConf).includes('only')) {
-                delete baseConf['only'];
-                break;
-            } else {
-                baseConf['only'] = `${currentService}:${functionName}`;
-            }
-        }
-    }
-    return baseConf;
-})()).then(() => {
-    console.log(`Your project ${projectName} has been deployed`);
-    return true;
-}).catch((err) => {
-    console.warn(`error has occurred! - ${err}`);
-});
+require('firebase-tools')
+    .deploy((() => ({project: projectName, token: token, cwd: __dirname}))())
+    .then(() => console.log(`Your project ${projectName} has been deployed`))
+    .catch((err) => console.warn(`error has occurred! - ${err}`));
